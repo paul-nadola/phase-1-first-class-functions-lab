ee8f1a8eaba9f3693489bc07a2f458f88a9c82b7
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-15 14:33:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+function createFareMultiplier(integer) {
+    return function(fare) {
+      return fare * integer;
+    }
+  }
+  
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers(drivers);
+}
+  
+
+      
+
